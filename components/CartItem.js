a263344aca1abc3b69346c05b650fbc9133b889b
inedu joshua <inedujoshua@gmail.com>
2022-07-18 15:01:35 +0100

    working on cart

--- components/CartItem.js
+++ components/CartItem.js
@@ -20,47 +20,76 @@ const CartItem = ({ cart, dispatch, item }) => {
   }, [item.quantity]);
 
   return (
-    <div key={item._id}>
-      <img width={150} height={150} src={item.images[0].url} alt={item.title} />
-
-      <p>{item.title}</p>
-      <p>{item.amount}</p>
-      <p>{item.description}</p>
-      <p>{item.category}</p>
-      {item.colors.map((color) => {
-        return <div key={color}>{color}</div>;
-      })}
-
-      <div>
-        <p>Amount: {item.quantity * item.amount}</p>
-        <p>quantity:{item.quantity}</p>
-        <div>
-          <span>increase product </span>
-          <button
-            disabled={item.quantity === 10 ? true : false}
-            onClick={() => dispatch(increase(cart, item._id))}
-          >
-            +
-          </button>
-        </div>
-        <div>
-          <span>increase product </span>
-          <button
-            onClick={() => dispatch(decrease(cart, item._id))}
-            disabled={item.quantity === 1 ? true : false}
-          >
-            -
-          </button>
-        </div>
-        <div>
-          <span></span>
-          <button
-            onClick={() => dispatch(deleteItem(cart, item._id, "ADD_CART"))}
-          >
-            Delete product{" "}
-          </button>
-        </div>
-      </div>
+    <div className="flex">
+      <table className="table-auto " key={item._id}>
+        <thead>
+          <tr className="w-full bg-gray-400">
+            <th>Item</th>
+            <th>Unit price</th>
+            <th>Qty</th>
+            <th>Sub total</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className="flex">
+                <img
+                  className="w-60 object-cover"
+                  src={item.images[0].url}
+                  alt={item.title}
+                />
+                <div>
+                  <p>{item.title}</p>
+                  {item.colors.map((color) => {
+                    return <div key={color}>{color}</div>;
+                  })}
+                </div>
+              </div>
+            </td>
+            <td>
+              <p>{item.amount}</p>
+            </td>
+            <td>
+              <p>quantity:{item.quantity}</p>
+              <div>
+                <div>
+                  <span>increase product </span>
+                  <button
+                    disabled={item.quantity === 10 ? true : false}
+                    onClick={() => dispatch(increase(cart, item._id))}
+                  >
+                    +
+                  </button>
+                </div>
+                <div>
+                  <span>increase product </span>
+                  <button
+                    onClick={() => dispatch(decrease(cart, item._id))}
+                    disabled={item.quantity === 1 ? true : false}
+                  >
+                    -
+                  </button>
+                </div>
+                <div>
+                  <span></span>
+                  <button
+                    onClick={() =>
+                      dispatch(deleteItem(cart, item._id, "ADD_CART"))
+                    }
+                  >
+                    Delete product{" "}
+                  </button>
+                </div>
+              </div>
+            </td>
+            <td>
+              {" "}
+              <p>Amount: {item.quantity * item.amount}</p>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
